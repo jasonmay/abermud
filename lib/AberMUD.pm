9985aeee7b1529dce849478c4ec5b87ca7752b16
Jason May <jason.a.may@gmail.com>
2009-05-10 21:20:36 -0400

    add Makefile.PL and AberMUD.pm

---
+++ lib/AberMUD.pm
@@ -0,0 +1,25 @@
+package AberMUD;
+
+1;
+
+__END__
+
+=head1 NAME
+
+AberMUD - A codebase for a quest-based MUD flavor
+
+=head1 SYNOPSIS
+
+  my $abermud = AberMUD->new;
+
+=head1 DESCRIPTION
+
+TODO TODO TODO TODO TODO 
+
+=head1 AUTHOR
+
+Jason May C<< <jason.a.amy@gmail.com >>
+
+=head1 LICENSE
+
+You may use this code under the same terms of Perl itself.
