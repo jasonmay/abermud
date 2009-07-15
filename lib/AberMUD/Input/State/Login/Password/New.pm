3371bb79137658211ae85d7f8f96853c30c4f2c8
Jason May <jason.a.may@gmail.com>
2009-07-14 21:42:23 -0400

    password states for new players

---
+++ lib/AberMUD/Input/State/Login/Password/New.pm
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+package AberMUD::Input::State::Login::Password::New;
+use Moose;
+extends 'MUD::Input::State';
+use AberMUD::Input::State::Game;
+use MUD::Input::State;
+
+sub run {
+    my $self = shift;
+    my ($you, $pass) = @_;
+
+        $you->confirmed_password($pass);
+
+        $you->shift_state;
+        return "Please verify this password by retyping it: ";
+}
+
+1;
