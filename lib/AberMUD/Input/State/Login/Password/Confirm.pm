b2e84b443955e18256935c2b07216ec8e5c780a8
Jason May <jason.a.may@gmail.com>
2009-07-15 22:10:09 -0400

    get password confirmation to work

--- lib/AberMUD/Input/State/Login/Password/Confirm.pm
+++ lib/AberMUD/Input/State/Login/Password/Confirm.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package AberMUD::Input::State::Login::Password::Confirm;
 use Moose;
+use AberMUD::Input::State::Login::Password::New;
 extends 'AberMUD::Input::State';
 
 has '+entry_message' => (
@@ -10,11 +11,19 @@ has '+entry_message' => (
 sub run {
     my $self = shift;
     my ($you, $pass) = @_;
+    my $output = q{};
 
-    $you->confirmed_password($pass);
+    if (crypt($pass, $you->name) eq $you->password) {
+        $you->shift_state;
+    }
+    else {
+        my $enter_new_password
+            = AberMUD::Input::State::Login::Password::New->new;
 
-    $you->shift_state;
-    return $you->input_state->[0]->entry_message;
+        $you->unshift_state($enter_new_password);
+        $output = "That did not match what you originally typed. Please try again.\n";
+    }
+    return $output . $you->input_state->[0]->entry_message;
 }
 
 1;
