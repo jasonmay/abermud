d73382ea3c82a77014400982c529acaddd95884b
Jason May <jason.a.may@gmail.com>
2009-07-29 22:55:41 -0400

    'who' command (tersely) shows players in the game

--- lib/AberMUD/Input/Command/Who.pm
+++ lib/AberMUD/Input/Command/Who.pm
@@ -6,11 +6,9 @@ extends 'AberMUD::Input::Command';
 has '+name' => ( default => 'who' );
 
 sub run {
-#    my $self = shift;
     my $you  = shift;
-#    my $args = shift;
-    return join(" " => map { ref || $_ } @_);
-#    return join "\n" => keys %{$you->universe->players_in_game};
+
+    return join "\n" => keys %{$you->universe->players_in_game};
 }
 
 no Moose;
