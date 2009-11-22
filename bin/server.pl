8b94e2c180568f3d5aaeb95b4814dd032b634b71
Jason May <jason.a.may@gmail.com>
2009-11-22 13:51:38 -0500

    add a server command to run

---
+++ bin/server.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use lib 'lib';
+use lib '../mud/lib';
+use AberMUD;
+
+require AberMUD::Container;
+
+my $c = AberMUD::Container->new->container;
+
+$c->fetch('app')->get->run;
