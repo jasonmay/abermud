e2116ecd70d6cd8e4098505a8b8d5c220e1a11c2
Jason May <jason.a.may@gmail.com>
2010-02-07 14:46:58 -0500

    forgot to add the take command

---
+++ lib/AberMUD/Input/Command/Take.pm
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+package AberMUD::Input::Command::Take;
+use Moose;
+use namespace::autoclean;
+extends 'AberMUD::Input::Command';
+
+my $command_name = lc __PACKAGE__;
+$command_name =~ s/.+:://; $command_name =~ s/\.pm//;
+override _build_name => sub { $command_name };
+
+sub run {
+    my $you  = shift;
+    my $args = shift;
+    my @args = split ' ', $args;
+
+    if (!@args) {
+        return "Take what?";
+    }
+    elsif (@args == 1) {
+        my @matching_objects = grep {
+            $_->location == $you->location
+            and lc($_->name) eq lc($args[0])
+        } @{ $you->universe->objects };
+
+        if (@matching_objects) {
+            if ($matching_objects[0]->can('held_by')) {
+                $matching_objects[0]->held_by($you);
+                $you->say(
+                    sprintf
+                    "%s picks up a %s.\n",
+                    $you->name, $matching_objects[0]->name
+                );
+                return sprintf("You take the %s.", $matching_objects[0]->name);
+            }
+        }
+    }
+    elsif (@args == 3 and lc($args[1]) eq 'from') {
+        return "Not supported yet.";
+    }
+    else {
+        return "That command syntax is not recognized.";
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
