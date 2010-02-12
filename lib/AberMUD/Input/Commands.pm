5e23383e88e86d12b465be096354d4f204417c8f
Jason May <jason.a.may@gmail.com>
2010-02-12 11:29:36 -0500

    module that generates methods for commands

---
+++ lib/AberMUD/Input/Commands.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+package AberMUD::Input::Commands;
+use Moose;
+#use namespace::autoclean;
+use Module::Pluggable
+    search_path => ['AberMUD::Input::Command'],
+    sub_name    => 'commands',
+    require     => 1;
+
+for (__PACKAGE__->commands) {
+    (my $command = lc $_) =~ s/.+:://;
+
+    Class::MOP::load_class($_);
+    my $module = $_;
+    __PACKAGE__->meta->add_method(
+        $command => sub { shift; no strict 'refs'; &{"${module}::run"}(@_) },
+    );
+    warn "$command loaded";
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
