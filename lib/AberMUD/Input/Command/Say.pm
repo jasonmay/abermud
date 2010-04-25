0deb02c9e84152d617bdeafb93972cc5608da59d
Jason May <jason.a.may@gmail.com>
2010-04-25 13:53:18 -0400

    'say' command

---
+++ lib/AberMUD/Input/Command/Say.pm
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+package AberMUD::Input::Command::Say;
+use Moose;
+use namespace::autoclean;
+extends 'AberMUD::Input::Command';
+
+has '+alias' => ( default => q['] );
+
+sub run {
+    my $you  = shift;
+    my $args  = shift;
+    my $output = q{};
+
+    $you->say(
+        $you->name . " says &+Y'$args'&*\n",
+        except => $you,
+    );
+
+    return "You say, &+Y'$args'";
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
