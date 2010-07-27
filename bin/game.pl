777f35dabf4ad4f88b30975b616591e9e5bdda32
Jason May <jason.a.may@gmail.com>
2010-07-27 13:10:36 -0400

    pass in kdb dir as an argument

--- bin/game.pl
+++ bin/game.pl
@@ -3,6 +3,15 @@ use strict;
 use warnings;
 use lib 'lib';
 use AberMUD::Container;
+use KiokuDB;
 
 my $c = AberMUD::Container->new->container;
+if (@ARGV) {
+    $c->fetch('directory')->get->kdb(
+        KiokuDB->connect(
+            "dbi:SQLite:dbname=$ARGV[0]"
+        )
+    )
+}
+
 $c->fetch('app')->get->run;
