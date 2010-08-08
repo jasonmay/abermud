346260623c30159bd8f94d56f8078ed91a4975b0
Jason May <jason.a.may@gmail.com>
2010-08-08 17:28:10 -0400

    shoo be doo be doo wop

--- lib/AberMUD/Input/Command/Directions.pm
+++ lib/AberMUD/Input/Command/Directions.pm
@@ -3,13 +3,13 @@ use AberMUD::OO::Commands;
 use AberMUD::Location::Util qw(directions);
 
 foreach my $direction (directions()) {
-    command($direction, priority => -10, sub {
-    my $you   = shift;
-	return "You are somehow nowhere." unless defined $you->location;
+    command $direction, priority => -10, sub {
+        my $you   = shift;
+        return "You are somehow nowhere." unless defined $you->location;
 
-        my $go_direction = "go_$direction";
-	return $you->$go_direction();
-    });
+            my $go_direction = "go_$direction";
+        return $you->$go_direction();
+    };
 };
 
 __PACKAGE__->meta->make_immutable;
