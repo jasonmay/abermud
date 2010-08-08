c5444ebf2318b366e0dd574ac42152deddab87a4
Jason May <jason.a.may@gmail.com>
2010-08-08 18:34:33 -0400

    remove excess comments

--- lib/AberMUD/Web/Controller/Players.pm
+++ lib/AberMUD/Web/Controller/Players.pm
@@ -21,11 +21,6 @@ Catalyst Controller.
 
 =cut
 
-#sub base : Chained('/') CaptureArgs(0) {
-#    my ( $self, $c, $id ) = @_;
-#    warn "DOOPY";
-#}
-
 sub id : PathPart('players/id') Chained('/') CaptureArgs(1) {
     my ( $self, $c, $id ) = @_;
 
@@ -89,56 +84,10 @@ sub reset_password : PathPart Chained('id') Args(0) {
 HTML
 }
 
-#sub base : Chained('/') CaptureArgs(0) { die }
-#
-#sub index : Path Local {
-#    my ( $self, $c ) = @_;
-#    $c->response->body("Players index :D");
-#}
-#
-#sub requires_id : Chained('base') Path('id') Args(0) {
-#    my ( $self, $c, $id ) = @_;
-#    $c->response->body("Requires ID");
-#}
-#
-#sub id : Chained('base') Local Args(1) {
-#    my ( $self, $c, $id ) = @_;
-#    $c->response->body("ID: $id");
-#}
-
-#sub id : Chained('base') PathPart('id')
-
-#sub chain_id : Chained('base') Args(1) {
-#    my ( $self, $c , $id) = @_;
-#
-#    my $scope = $c->model('dir')->new_scope();
-#    my $p = $c->model('dir')->lookup("player-$id") or do {
-#        $c->response->body(qq|Player '$id' doesn't exist.|);
-#        return;
-#    };
-#
-#    $c->stash->{player} = $p;
-#
-#    my $data = join "<br />\n", map { ucfirst() . ': ' . $p->$_ }
-#        qw/name level score damage armor basestrength basemana/;
-#
-#}
-#
-#sub id :Chained('chain_id') :PathPart('id') :Args(0) {
-#    my ( $self, $c) = @_;
-#    $c->response->body('hmmmm?');
-#}
-#
-#sub reset_password :Chained('id') :PathPart('reset_password') :Args(0) {
-#    my ($self, $c) = @_;
-#
-#    $c->response->body('Not implemented yet');
-#}
-
 sub default :Local {
     my ($self, $c) = @_;
 
-    $c->response->body('404. your shit is clearly broken. take a break!');
+    $c->response->body('404!');
 }
 
 =head1 AUTHOR
