d2ac5fa1817b8cc3b176cddfcca25770bd9b465c
Jason May <jason.a.may@gmail.com>
2011-01-10 10:41:09 -0500

    prompt and save

--- lib/AberMUD/Input/Command/Prompt.pm
+++ lib/AberMUD/Input/Command/Prompt.pm
@@ -2,6 +2,14 @@ package AberMUD::Input::Command::Prompt;
 use Moose;
 use AberMUD::OO::Commands;
 
+command prompt => sub {
+    my $you     = shift;
+    my $args    = shift;
+
+    $you->prompt($args);
+    return "Your prompt has been changed.";
+};
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
--- lib/AberMUD/Input/Command/Save.pm
+++ lib/AberMUD/Input/Command/Save.pm
@@ -2,6 +2,14 @@ package AberMUD::Input::Command::Save;
 use Moose;
 use AberMUD::OO::Commands;
 
+command save => sub {
+    my $you     = shift;
+    my $args    = shift;
+
+    $you->save_data();
+    return "Saved!";
+};
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
