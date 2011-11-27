0979e7972e21a4a556f356f2d1af015bd74101a2
Jason May <jason.a.may@gmail.com>
2011-11-26 23:40:18 -0500

    Update the 'examine' command to support the new argument layout

--- lib/AberMUD/Input/Command/Examine.pm
+++ lib/AberMUD/Input/Command/Examine.pm
@@ -3,15 +3,14 @@ package AberMUD::Input::Command::Examine;
 use AberMUD::OO::Commands;
 
 command examine => sub {
-    my $you  = shift;
-    my $args = shift;
+    my ($universe, $you, $args) = @_;
     my @args = split ' ', $args;
 
     if (!@args) {
         return "Examine what?";
     }
     else {
-        my $in_game = $you->universe->identify($you->location, $args[0])
+        my $in_game = $universe->identify($you->location, $args[0])
             or return "Nothing of that name is here.";
 
         return $in_game->examine_description
