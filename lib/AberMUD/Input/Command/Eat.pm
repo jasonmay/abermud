d027f108bf204ea49f1d0edce90fe0b2ef4ec71a
Jason May <jason.a.may@gmail.com>
2011-12-31 17:58:11 -0500

    Stop carrying the eaten object

--- lib/AberMUD/Input/Command/Eat.pm
+++ lib/AberMUD/Input/Command/Eat.pm
@@ -16,6 +16,8 @@ command eat => sub {
 
         $you->current_strength($you->current_strength + $object->nutrition);
         $object->location($universe->eaten_location);
+        $you->_carrying->remove($object);
+        $object->_stop_being_held;
 
         return "You eat the " . $object->name . ".";
     }
