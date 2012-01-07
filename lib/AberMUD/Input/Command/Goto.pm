cb94a458aeffb6437306a03ac515215c7985be07
Jason May <jason.a.may@gmail.com>
2012-01-07 17:06:46 -0500

    Make the goto command a little more useful

--- lib/AberMUD/Input/Command/Goto.pm
+++ lib/AberMUD/Input/Command/Goto.pm
@@ -4,16 +4,23 @@ use AberMUD::OO::Commands;
 
 command goto => sub {
     my ($self, $e) = @_;
-    (my $loc_id = lc $e->arguments) =~ s/\W//g;
+    my ($name, $zone) = split '@', $e->arguments, 2;
+    return "Please specify a mobile." unless $name;
+    return "Please specify a zone." unless $zone;
 
-    my $loc = $e->universe->storage->lookup("location-$loc_id");
-
-    if (!$loc) {
-        return "Could not find that location.";
+    my @mobs = $e->universe->get_mobiles;
+    for my $mob (@mobs) {
+        next unless $mob->zone->name eq $zone;
+        next unless $mob->name eq $name;
+        $e->universe->change_location($e->player, $mob->location);
+        my $look_event = AberMUD::Event::Command->new(
+            universe  => $e->universe,
+            player    => $e->player,
+            arguments => '',
+        );
+        return $self->trigger_command('look', $look_event);
     }
-
-    $e->player->change_location($loc);
-    return $e->universe->look;
+    return "I could not find $name in $zone.";
 };
 
 __PACKAGE__->meta->make_immutable;
