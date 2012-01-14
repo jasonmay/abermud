93a18b006b5efeddb3b2c37283a4f84a48717ace
Jason May <jason.a.may@gmail.com>
2012-01-14 09:45:49 -0500

    Make the 'goto' game command a little more useful
    It supports objects and players now.

--- lib/AberMUD/Input/Command/Goto.pm
+++ lib/AberMUD/Input/Command/Goto.pm
@@ -5,14 +5,26 @@ use AberMUD::OO::Commands;
 command goto => sub {
     my ($self, $e) = @_;
     my ($name, $zone) = split '@', $e->arguments, 2;
-    return "Please specify a mobile." unless $name;
-    return "Please specify a zone." unless $zone;
+    return "Please specify something." unless $name;
 
-    my @mobs = $e->universe->get_mobiles;
-    for my $mob (@mobs) {
-        next unless $mob->zone->name eq $zone;
-        next unless $mob->name eq $name;
-        $e->universe->change_location($e->player, $mob->location);
+    my @things = ();
+    push @things, $e->universe->get_objects, $e->universe->get_mobiles;
+    push @things, $e->universe->player_list unless $zone;
+    for my $thing (@things) {
+        if ($thing->can('zone') and defined $thing->zone) {
+            next unless $thing->zone->name eq $zone;
+        }
+        next unless $thing->name eq $name;
+        warn $thing->name . '?';
+        warn $thing->location;
+        warn $thing->contained_by if $thing->getable;
+        next unless $thing->location;
+        warn $thing->name;
+        if ($thing == $e->player) {
+            return "That's you!";
+        }
+
+        $e->universe->change_location($e->player, $thing->location);
         my $look_event = AberMUD::Event::Command->new(
             universe  => $e->universe,
             player    => $e->player,
